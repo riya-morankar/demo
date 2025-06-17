ab4153fe4e3fdc6cc8033e9b455e0e4e87f114ad
riya-morankar <riya_milind_morankar@baxter.com>
2025-06-17 12:04:28 +0530

    Update generate_excel.py

--- .github/scripts/generate_excel.py
+++ .github/scripts/generate_excel.py
@@ -5,33 +5,32 @@ from openpyxl import Workbook, load_workbook
 
 event_path = os.getenv('GITHUB_EVENT_PATH')
 with open(event_path, 'r') as f:
-  event = json.load(f)
-
+    event = json.load(f)
 pr = event.get("pull_request")
 if not pr:
-  raise ValueError("No pull-request data found")
+    raise ValueError("No pull_request data found.")
 
 req_id = pr.get("number")
 title = pr.get("title")
-author = pr.get("user",{}).get("login")
-source_target = f"{pr.get('head',{}).get('ref')} to {pr.get('base', {}).get('ref')}"
+author = pr.get("user", {}).get("login")
+source_target = f"{pr.get('head', {}).get('ref')} → {pr.get('base', {}).get('ref')}"
 
-approver_list = [r.get("login") for r in pr.get("requested_reviewers",[])]
-approvers = ",".join(approver_list) if approver_list else N/A
+approvers_list = [r.get("login") for r in pr.get("requested_reviewers", [])]
+approvers = ", ".join(approvers_list) if approvers_list else "N/A"
 
 merged_at = pr.get("merged_at")
-merged_data = merged_at[:10] if merged_at else datetime.now().strftime(%Y-%m-%d)
+merged_date = merged_at[:10] if merged_at else datetime.now().strftime("%Y-%m-%d")
 
-excel_file = "pull-request.xlsx"
-if(os.path.exists(excel_file)):
-  wb=load_workbook(excel_file)
-  ws=wb.active
+excel_file = "pull_requests.xlsx"
+if os.path.exists(excel_file):
+    wb = load_workbook(excel_file)
+    ws = wb.active
 else:
-  wb=Workbook()
-  ws=wb.active
-  ws.append(["Req ID", "Title", "Author", "Approver", "Source/target", "Date"])
+    wb = Workbook()
+    ws = wb.active
+    ws.append(["Req ID", "Title", "Author", "Approvers", "Source → Target", "Date"])
 
 ws.append([req_id, title, author, approvers, source_target, merged_date])
 
-ws.save(excel_file)
+wb.save(excel_file)
 print(f"Logged PR #{req_id}")
