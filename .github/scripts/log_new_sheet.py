b4f30df02ec7341e95645bbb414601c915686c03
riya-morankar <riya_milind_morankar@baxter.com>
2025-06-18 13:39:12 +0530

    Update log_new_sheet.py

--- .github/scripts/log_new_sheet.py
+++ .github/scripts/log_new_sheet.py
@@ -16,7 +16,7 @@ author = pr.get("user", {}).get("login")
 source_branch = pr.get("head", {}).get("ref")
 
 # action = "Merged" if pr.get("merged") else "Squashed"
-action = "Merged" if pr.get("merged") and not pr.get("squash") else "Squashed"
+action = "Merged" if pr.get("merge_status") == "MERGED" else "Squashed"
 
 # action = "Merged" if pr.get("merged") else "Squashed"
 # if pr.get("squash"):
